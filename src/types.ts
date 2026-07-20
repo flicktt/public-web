67dc2e2223e2bcade412939076c9ceaf231633d6
siddgrg <sidgurung97@gmail.com>
2026-07-20 09:40:07 +0100

    astro 7/vite 8 upgrade, schedule refactor and design update

--- src/types.ts
+++ src/types.ts
@@ -2,39 +2,39 @@ import type { AstroComponentFactory } from "astro/runtime/server/index.js";
 import type { SvgComponent } from "astro/types";
 
 export type Page =
-	| "home"
-	| "offerings"
-	| "compete"
-	| "kids"
-	| "membership"
-	| "about"
-	| "coaches";
+  | "home"
+  | "offerings"
+  | "compete"
+  | "kids"
+  | "membership"
+  | "about"
+  | "coaches";
 
 export type PageConfig = {
-	title: string;
+  title: string;
 };
 
 export type FaqConfig = {
-	title: string;
-	Content: AstroComponentFactory | string;
+  title: string;
+  Content: AstroComponentFactory | string;
 };
 
 export type PageTabConfig = {
-	label: string;
-	contentId: string;
-	Content: AstroComponentFactory;
+  label: string;
+  contentId: string;
+  Content: AstroComponentFactory;
 };
 
 export type PointsRowConfig = {
-	Icon: SvgComponent;
-	text: string;
+  Icon: SvgComponent;
+  text: string;
 }[];
 
 export type CoachProfileConfig = {
-	img: string;
-	name: string;
-	title: "head" | "coach";
-	description: string[];
+  img: string;
+  name: string;
+  title: "head" | "coach";
+  description: string[];
 };
 
 export interface ContainerProps extends astroHTML.JSX.HTMLAttributes {
@@ -42,3 +42,28 @@ export interface ContainerProps extends astroHTML.JSX.HTMLAttributes {
   main?: boolean;
   fullMaxWidth?: boolean;
 }
+
+export type ScheduleDayConfig =
+  | {
+      type: "clubTable";
+      isPeak?: boolean;
+      hasRobot?: boolean;
+      hours: number;
+    }
+  | {
+      type: "group";
+      lvl: "3-4-5" | "2-3" | "all" | "1-2" | "u18-1-2" | "u18-3-4" | "3-4";
+      hours: number;
+    }
+  | {
+      type: "clubLeague";
+      matching: "open" | "grouped";
+      hours: number;
+    };
+
+export type ScheduleDayProps = {
+  day: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun";
+  blocks: ScheduleDayConfig[];
+};
+
+export type ScheduleConfig = ScheduleDayProps[];
