cfabc802c9bbf6104a41dea444c45de7c7363f6d
siddgrg <sidgurung97@gmail.com>
2026-07-21 18:14:56 +0100

    home page membership row position update

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const hasPaddingClass = (
-  classString: string,
-  options?: { onlyX?: boolean; onlyY?: boolean },
-) => {
-  const regex = options?.onlyX
-    ? /(?:^|\s)p(?:x|r|l)?-[^\s]+(?:\s|$)/
-    : options?.onlyY
-      ? /(?:^|\s)p(?:y|t|b)?-[^\s]+(?:\s|$)/
-      : /(?:^|\s)p(?:x|y|r|l|t|b)?-[^\s]+(?:\s|$)/;
-
-  return regex.test(classString);
-};
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
