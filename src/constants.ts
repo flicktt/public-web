60f7a538b50b0faea0003e2a9051aa820f3058ea
siddgrg <sidgurung97@gmail.com>
2026-08-06 15:12:03 +0100

    Change IG logo to svg, CoachRow, title prop for Coaches, constants file

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const SUPPORT_EMAIL = "services@flicktt.club";
+
+export const COACH_TITLES: Record<
+  string,
+  { singular: string; plural: string }
+> = {
+  head: { singular: "Head Coach", plural: "Head Coaches" },
+  coach: { singular: "Club Coach", plural: "Club Coaches" },
+};
