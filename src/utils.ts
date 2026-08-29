5240f7f9abe191069fe266ff617fd14303620188
siddgrg <sidgurung97@gmail.com>
2026-08-29 15:02:55 +0100

    league, tournament, u18 tournament, workshops, mem benefit content update

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getDateString(date: Date) {
+  return date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+}
